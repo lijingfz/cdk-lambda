0f24db68beaf5ee0b5a36baca4dfee2d6d64d832
lijingfz <lijingfz@163.com>
2024-08-21 10:27:42 +0800

    Update cdk-lambda.ts

--- bin/cdk-lambda.ts
+++ bin/cdk-lambda.ts
@@ -7,5 +7,6 @@ new CdkLambdaStack(app, 'MyCloudFrontStack',{
    // bucketName: 'minimaxzoompic',
     env: {
         region: 'us-east-1',
+        // 这是必须是 us-east-1 区域，原因是 edge@lambda函数 只能在美东1 被调用
       },
 });
